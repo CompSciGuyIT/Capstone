f07222997a0449fda847e254b3c2f49335dc0fea
Gary Murphy <compsciguy.it@gmail.com>
2020-05-06 17:57:59 +1000

    Changed some css on a button to resemble one on Blue Light's website.

--- Demo/script.js
+++ Demo/script.js
@@ -14,28 +14,28 @@ mariadb.createConnection({
     conn.query('CREATE TABLE IF NOT EXISTS members (firstName CHAR(50), lastName CHAR(50), email CHAR(50))');
 
     document.getElementById("create-btn").addEventListener("click", () => {
-        conn.query('INSERT INTO members (firstName, lastName, email) VALUES ("Suzie", "Q", "suzie@email.com")')
+        conn.query('INSERT INTO members (firstName, lastName, email) VALUES ("Mark", "Wednesday", "mark@email.com")')
         .then(res => {
             console.log(res);
         });       
     }, false);
 
     document.getElementById("retrieve-btn").addEventListener("click", () => {
-        conn.query('SELECT * FROM members WHERE firstName="Suzie"')
+        conn.query('SELECT * FROM members WHERE firstName="Mark"')
         .then(res => {
             console.log(res);
         });
     }, false);
     
     document.getElementById("update-btn").addEventListener("click", () => {
-        conn.query('UPDATE members SET email="q.suz@email.com" WHERE firstName="Suzie"')
+        conn.query('UPDATE members SET email="wednesday@email.com" WHERE firstName="Mark"')
         .then(res => {
             console.log(res);
         });
     }, false);
     
     document.getElementById("delete-btn").addEventListener("click", () => {
-        conn.query('DELETE FROM members WHERE firstName="Suzie"')
+        conn.query('DELETE FROM members WHERE firstName="Mark"')
         .then(res => {
             console.log(res);
         });
