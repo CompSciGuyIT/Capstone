f981beaf56641024c1755c3dcef00559352e9d32
Gary Murphy <compsciguy.it@gmail.com>
2020-07-31 20:55:11 +1000

    Boilerplate for Prototype Consider this to be the main code for the app Any changes made should first be done in a branch, before merging into the master The master should never contain broken code

---
+++ Prototype/main.js
@@ -0,0 +1,54 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
+const url = require('url')
+const mariadb = require('mariadb')
+
+const connection = mariadb.createPool({
+    host: '127.0.0.1', 
+    user: 'root', 
+    password: '', 
+    database: 'blue_light',
+    connectionLimit: 5 
+});
+
+function createWindow() {
+    // Create the browser window.
+    let mainWindow = new BrowserWindow ({
+        width: 1280,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format ({
+        pathname: path.join(__dirname, 'views', 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    mainWindow.webContents.on("new-window",function(event,url){
+        event.preventDefault();
+        shell.openExternal(url);
+    })
+
+    // or Ctrl-Shift-I when app is running
+    mainWindow.webContents.openDevTools()
+} 
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Connect to and query database
+connection.getConnection()
+    .then(conn => {
+        console.log("connected ! connection id is " + conn.threadId);
+        conn.release(); //release to pool
+        console.log("released connection to pool");
+    })
+    .catch(err => {
+        console.log("not connected due to error: " + err);
+    });
