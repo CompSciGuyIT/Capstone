67c7236c641b3512a8243b3c2f0fb298afa6e426
ducanhrevilo <ducanhrevilo@ducanhs-MacBook-Pro.local>
2020-09-13 23:33:19 +1000

    setup for 5 tables
    push data from database to 5 tables

--- ok/public/js/main.js
+++ ok/public/js/main.js
@@ -4,10 +4,21 @@ $(document).ready(function () {
 		window.location.href = `${tableName}`
 	});
 
-	// table 1
+///////////////TABLE 1
 	$('#table1').DataTable({
 		ajax: '/get-data-table1',
-		columns: [{
+		columns: [
+			{
+				data: null,
+				render: function (data) {
+					return `
+						<div class="d-flex align-items-center">
+							<a href="/update1/${data['ID']}" class="btn btn-dark mr-2">Update</a>
+							<button class="btn btn-danger js--btn-delete1" data-id="${data['ID']}">Delete</button>
+						</div>
+					`;
+				}
+			},{
 				data: null,
 				render: function(data) {
 					return data['ID']
@@ -283,23 +294,692 @@ $(document).ready(function () {
 					return data['ON_BC_LIST']
 				},
 			},
+			
+		],
+	});
+	
+
+
+
+
+
+
+
+
+////////////////TABLE2
+	$('#table2').DataTable({
+		ajax: '/get-data-table2',
+		columns:[
 			{
 				data: null,
 				render: function (data) {
 					return `
 						<div class="d-flex align-items-center">
 							<a href="/update1/${data['ID']}" class="btn btn-dark mr-2">Update</a>
-							<button class="btn btn-danger js--btn-delete" data-id="${data['ID']}">Delete</button>
+							<button class="btn btn-danger js--btn-delete2" data-id="${data['ID']}">Delete</button>
 						</div>
 					`;
 				}
+			},{
+			data: null,
+			render: function(data) {
+				return data['ID']
 			},
-		],
-	});
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['BRANCH']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['MEMBERSHIP']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['REGION']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['MAIL_CONTACT']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['ADDRESS_LINE_1_STATION/BRANCH']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['ADDRESS_LINE_2_PO_BOXSTREET']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['ADDRESS_LINE_3_SUBURB_TOWN']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['COORDINATOR']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['PHONE']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['FAX']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['EMAIL']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['HOME']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['MOBILE']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['OTHER_CONTACTS']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['PRESIDENT']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['PRES_PHONE']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['PRES_ADDRESS']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['VICE_PRESIDENT']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['V/P_Phone']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['V/P_Address']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['SECRETARY']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Sec_Phone']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Sec_Address']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['TREASURER']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Tres_Phone']
+			},
+		},
+		{
+			data: null,
+			render: function (data) {
+				return data['Tres_Address']
+			}
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check59']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['2008_A']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check63']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Affiliated_Since']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Text69']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Text72']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check75']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check79']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Text81']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Text83']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check85']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Text87']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Text89']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Combo91']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check94']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check100']
+			},
+		},
+		{
+			data: null,
+			render: function(data) {
+				return data['Check102']
+			},
+		},
+		
 
-	$(document).on('click', '.js--btn-delete', function () {
+	],
+});
+
+
+
+
+
+
+
+
+
+
+/////////////////TABLE3
+$('#table3').DataTable({
+	ajax: '/get-data-table3',
+	columns:[
+		{
+			data: null,
+			render: function (data) {
+				return `
+					<div class="d-flex align-items-center">
+						<a href="/update1/${data['ID']}" class="btn btn-dark mr-2">Update</a>
+						<button class="btn btn-danger js--btn-delete3" data-id="${data['ID']}">Delete</button>
+					</div>
+				`;
+			}
+		},{
+		data: null,
+		render: function(data) {
+			return data['ID']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['BRANCH']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['MEMBERSHIP']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['REGION']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['MAIL_CONTACT']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['ADDRESS_LINE_1_STATION/BRANCH']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['ADDRESS_LINE_2_PO_BOXSTREET']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['ADDRESS_LINE_3_SUBURB_TOWN']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['COORDINATOR']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['PHONE']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['FAX']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['EMAIL']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['HOME']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['MOBILE']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['OTHER_CONTACTS']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['PRESIDENT']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['PRES_PHONE']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['PRES_ADDRESS']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['VICE_PRESIDENT']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['V/P_Phone']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['V/P_Address']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['SECRETARY']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Sec_Phone']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Sec_Address']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['TREASURER']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Tres_Phone']
+		},
+	},
+	{
+		data: null,
+		render: function (data) {
+			return data['Tres_Address']
+		}
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Text72']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Check75']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Check79']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Text81']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Text83']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Check85']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Text87']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Text89']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Text91']
+		},
+	},
+
+
+
+
+	
+],
+});
+
+
+
+
+
+
+
+
+
+/////////////////TABLE4
+$('#table4').DataTable({
+	ajax: '/get-data-table4',
+	columns:[
+		{
+			data: null,
+			render: function (data) {
+				return `
+					<div class="d-flex align-items-center">
+						<a href="/update1/${data['ID']}" class="btn btn-dark mr-2">Update</a>
+						<button class="btn btn-danger js--btn-delete4" data-id="${data['ID']}">Delete</button>
+					</div>
+				`;
+			}
+		},{
+		data: null,
+		render: function(data) {
+			return data['Key']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['BRANCH']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['First_Name']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Surname']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Year']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Combo22']
+		},
+	},
+],
+});
+
+
+
+
+
+
+///////////////////TABLE5
+$('#table5').DataTable({
+	ajax: '/get-data-table5',
+	columns:[
+		{
+			data: null,
+			render: function (data) {
+				return `
+					<div class="d-flex align-items-center">
+						<a href="/update1/${data['ID']}" class="btn btn-dark mr-2">Update</a>
+						<button class="btn btn-danger js--btn-delete5" data-id="${data['ID']}">Delete</button>
+					</div>
+				`;
+			}
+		},{
+		data: null,
+		render: function(data) {
+			return data['ID']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Title']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['FirstName']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Surname']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Region']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Address1']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Address2']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['State']
+		},
+	},
+	{
+		data: null,
+		render: function(data) {
+			return data['Postcode']
+		},
+	},
+	
+],
+});
+
+
+
+
+	$(document).on('click', '.js--btn-delete1', function () {
 		const id = $(this).data('id')
-		const r = confirm("Are you sure?");
+		const r = confirm("Are you sure delete volunteer?");
 		if (r == true) {
 			$.ajax({
 				url: '/delete1',
@@ -313,4 +993,75 @@ $(document).ready(function () {
 			});
 		}
 	})
+
+	$(document).on('click', '.js--btn-delete2', function () {
+		const id = $(this).data('id')
+		const r = confirm("Are you sure delete affiliate member?");
+		if (r == true) {
+			$.ajax({
+				url: '/delete2',
+				type: 'post',
+				data: {
+					id,
+				},
+				success: function (result) {
+					window.location.reload()
+				}
+			});
+		}
+	})
+
+	$(document).on('click', '.js--btn-delete3', function () {
+		const id = $(this).data('id')
+		const r = confirm("Are you sure delete associate member?");
+		if (r == true) {
+			$.ajax({
+				url: '/delete3',
+				type: 'post',
+				data: {
+					id,
+				},
+				success: function (result) {
+					window.location.reload()
+				}
+			});
+		}
+	})
+
+
+	$(document).on('click', '.js--btn-delete4', function () {
+		const id = $(this).data('id')
+		const r = confirm("Are you sure delete reward?");
+		if (r == true) {
+			$.ajax({
+				url: '/delete4',
+				type: 'post',
+				data: {
+					id,
+				},
+				success: function (result) {
+					window.location.reload()
+				}
+			});
+		}
+	})
+	
+	$(document).on('click', '.js--btn-delete5', function () {
+		const id = $(this).data('id')
+		const r = confirm("Are you sure delete qps_agm_invites?");
+		if (r == true) {
+			$.ajax({
+				url: '/delete5',
+				type: 'post',
+				data: {
+					id,
+				},
+				success: function (result) {
+					window.location.reload()
+				}
+			});
+		}
+	})
+
+	
 });
--- ok/routes/index.js
+++ ok/routes/index.js
@@ -17,12 +17,12 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page' });
 });
 
-// table 1
+// table 111111111111111111111
 router.get('/table1', function (req, res) {
   res.render('table1');
 });
 router.get('/get-data-table1', function (req, res) {
-  con.query("SELECT * FROM table1", function (err, result) {
+  con.query("SELECT * FROM volunteerr", function (err, result) {
     if (err) throw err;
     res.send({
       data: result,
@@ -46,15 +46,15 @@ router.post('/create1', function (req, res) {
       `'${element}'`
     ]
   }
-  const sql = "INSERT INTO table1 (" + colArray.join(', ') + ") VALUES (" + dataArray.join(', ') + ")";
+  const sql = "INSERT INTO volunteerr (" + colArray.join(', ') + ") VALUES (" + dataArray.join(', ') + ")";
   con.query(sql, function (err) {
     if (err) throw err;
-    console.log("1 record inserted");
+    console.log("1 record inserted into volunteerr table");
     res.redirect('/table1')
   });
 });
 router.get('/update1/:id', function (req, res) {
-  const sql = "SELECT * FROM table1 WHERE ID = '" + req.params.id + "'";
+  const sql = "SELECT * FROM volunteerr WHERE ID = '" + req.params.id + "'";
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.render('update1', { title: 'Update', data: result[0] });
@@ -69,7 +69,7 @@ router.post('/update1', function (req, res) {
       `${key} = '${element}'`
     ]
   }
-  const sql = "UPDATE table1 SET " + colArray.join(', ') + " WHERE ID = '" + req.body['ID'] + "'";
+  const sql = "UPDATE volunteerr SET " + colArray.join(', ') + " WHERE ID = '" + req.body['ID'] + "'";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log(result.affectedRows + " record(s) updated");
@@ -78,20 +78,322 @@ router.post('/update1', function (req, res) {
 });
 router.post('/delete1', function (req, res) {
   const { id } = req.body
-  const sql = "DELETE FROM table1 WHERE ID = '" + id + "'";
+  const sql = "DELETE FROM volunteerr WHERE ID = '" + id + "'";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Number of records deleted: " + result.affectedRows);
+    console.log("Number of records deleted in volunteer table: " + result.affectedRows);
     res.send('deleted')
   });
 });
 
-// table 2
 
-// table 3
 
-// table 4
 
-// table 5
+
+
+
+
+// table 222222222222222222222
+router.get('/table2', function (req, res) {
+  res.render('table2');
+});
+router.get('/get-data-table2', function (req, res) {
+  con.query("SELECT * FROM affiliate", function (err, result) {
+    if (err) throw err;
+    res.send({
+      data: result,
+    })
+  });
+});
+router.get('/create2', function (req, res) {
+  res.render('create2', { title: 'Create' });
+});
+router.post('/create2', function (req, res) {
+  let colArray = []
+  let dataArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      key
+    ]
+    dataArray = [
+      ...dataArray,
+      `'${element}'`
+    ]
+  }
+  const sql = "INSERT INTO affiliate (" + colArray.join(', ') + ") VALUES (" + dataArray.join(', ') + ")";
+  con.query(sql, function (err) {
+    if (err) throw err;
+    console.log("1 record inserted into affiliate table");
+    res.redirect('/table2')
+  });
+});
+router.get('/update2/:id', function (req, res) {
+  const sql = "SELECT * FROM affiliate WHERE ID = '" + req.params.id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render('update2', { title: 'Update', data: result[0] });
+  });
+});
+router.post('/update2', function (req, res) {
+  let colArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      `${key} = '${element}'`
+    ]
+  }
+  const sql = "UPDATE affiliate SET " + colArray.join(', ') + " WHERE ID = '" + req.body['ID'] + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+    res.redirect('/table2')
+  });
+});
+router.post('/delete2', function (req, res) {
+  const { id } = req.body
+  const sql = "DELETE FROM affiliate WHERE ID = '" + id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err; 
+    console.log("Number of records deleted in affiliate table : " + result.affectedRows);
+    res.send('deleted')
+  });
+});
+
+
+
+
+
+
+
+
+
+
+///////////////////////// table 3
+router.get('/table3', function (req, res) {
+  res.render('table3');
+});
+router.get('/get-data-table3', function (req, res) {
+  con.query("SELECT * FROM associate", function (err, result) {
+    if (err) throw err;
+    res.send({
+      data: result,
+    })
+  });
+});
+router.get('/create3', function (req, res) {
+  res.render('create3', { title: 'Create' });
+});
+router.post('/create3', function (req, res) {
+  let colArray = []
+  let dataArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      key
+    ]
+    dataArray = [
+      ...dataArray,
+      `'${element}'`
+    ]
+  }
+  const sql = "INSERT INTO associate (" + colArray.join(', ') + ") VALUES (" + dataArray.join(', ') + ")";
+  con.query(sql, function (err) {
+    if (err) throw err;
+    console.log("1 record inserted into associate table");
+    res.redirect('/table3')
+  });
+});
+router.get('/update3/:id', function (req, res) {
+  const sql = "SELECT * FROM associate WHERE ID = '" + req.params.id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render('update3', { title: 'Update', data: result[0] });
+  });
+});
+router.post('/update3', function (req, res) {
+  let colArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      `${key} = '${element}'`
+    ]
+  }
+  const sql = "UPDATE associate SET " + colArray.join(', ') + " WHERE ID = '" + req.body['ID'] + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+    res.redirect('/table3')
+  });
+});
+router.post('/delete3', function (req, res) {
+  const { id } = req.body
+  const sql = "DELETE FROM associate WHERE ID = '" + id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err; 
+    console.log("Number of records deleted in associate table : " + result.affectedRows);
+    res.send('deleted')
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+/////////////// TABLE 4
+router.get('/table4', function (req, res) {
+  res.render('table4');
+});
+router.get('/get-data-table4', function (req, res) {
+  con.query("SELECT * FROM award_recipients", function (err, result) {
+    if (err) throw err;
+    res.send({
+      data: result,
+    })
+  });
+});
+router.get('/create4', function (req, res) {
+  res.render('create4', { title: 'Create' });
+});
+router.post('/create4', function (req, res) {
+  let colArray = []
+  let dataArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      key
+    ]
+    dataArray = [
+      ...dataArray,
+      `'${element}'`
+    ]
+  }
+  const sql = "INSERT INTO award_recipients (" + colArray.join(', ') + ") VALUES (" + dataArray.join(', ') + ")";
+  con.query(sql, function (err) {
+    if (err) throw err;
+    console.log("1 record inserted into award_recipients table");
+    res.redirect('/table4')
+  });
+});
+router.get('/update4/:id', function (req, res) {
+  const sql = "SELECT * FROM award_recipients WHERE ID = '" + req.params.id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render('update4', { title: 'Update', data: result[0] });
+  });
+});
+router.post('/update4', function (req, res) {
+  let colArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      `${key} = '${element}'`
+    ]
+  }
+  const sql = "UPDATE award_recipients SET " + colArray.join(', ') + " WHERE ID = '" + req.body['ID'] + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+    res.redirect('/table4')
+  });
+});
+router.post('/delete4', function (req, res) {
+  const { id } = req.body
+  const sql = "DELETE FROM award_recipients WHERE ID = '" + id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err; 
+    console.log("Number of records deleted in award_recipients table : " + result.affectedRows);
+    res.send('deleted')
+  });
+});
+
+
+
+
+
+
+
+
+//////////////////TABLE5
+
+router.get('/table5', function (req, res) {
+  res.render('table5');
+});
+router.get('/get-data-table5', function (req, res) {
+  con.query("SELECT * FROM qps_agm_invites", function (err, result) {
+    if (err) throw err;
+    res.send({
+      data: result,
+    })
+  });
+});
+router.get('/create5', function (req, res) {
+  res.render('create5', { title: 'Create' });
+});
+router.post('/create5', function (req, res) {
+  let colArray = []
+  let dataArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      key
+    ]
+    dataArray = [
+      ...dataArray,
+      `'${element}'`
+    ]
+  }
+  const sql = "INSERT INTO qps_agm_invites (" + colArray.join(', ') + ") VALUES (" + dataArray.join(', ') + ")";
+  con.query(sql, function (err) {
+    if (err) throw err;
+    console.log("1 record inserted into qps_agm_invites table");
+    res.redirect('/table4')
+  });
+});
+router.get('/update5/:id', function (req, res) {
+  const sql = "SELECT * FROM qps_agm_invites WHERE ID = '" + req.params.id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.render('update5', { title: 'Update', data: result[0] });
+  });
+});
+router.post('/update5', function (req, res) {
+  let colArray = []
+  for (const key in req.body) {
+    const element = req.body[key];
+    colArray = [
+      ...colArray,
+      `${key} = '${element}'`
+    ]
+  }
+  const sql = "UPDATE qps_agm_invites SET " + colArray.join(', ') + " WHERE ID = '" + req.body['ID'] + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+    res.redirect('/table5')
+  });
+});
+router.post('/delete5', function (req, res) {
+  const { id } = req.body
+  const sql = "DELETE FROM qps_agm_invites WHERE ID = '" + id + "'";
+  con.query(sql, function (err, result) {
+    if (err) throw err; 
+    console.log("Number of records deleted in qps_agm_invites table : " + result.affectedRows);
+    res.send('deleted')
+  });
+});
 
 module.exports = router;
